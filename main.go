710eeff11ff15c48f65d709cb5221047f54a9864
neossoftware <mario.hidalgom@yahoo.com.mx>
2020-05-24 13:21:49 -0500

    cambios

--- main.go
+++ main.go
@@ -3,15 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	greeter := gophersGreeter{"Hello", "gophers"}
-	greeter.greet("Hello", "gophers")
-}
-
-type gophersGreeter struct {
-	how string
-	who string
-}
-
-func (greeter gophersGreeter) greet(how string, who string) {
-	fmt.Printf("%s %s!", greeter.how, greeter.who)
+	var i int
+	i=45
+	fmt.Printf("Hello World Go!! %d" , i)
 }
